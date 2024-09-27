c1395dee5fd47bb3f6ebe03ee9f80f21dd294fd9
Alessio Tommasi <alessiotom@live.it>
2024-09-27 17:49:50 +0200

    Before upload assignement

---
+++ Lab12/README.md
@@ -0,0 +1,34 @@
+# Assignment Report  
+**Assignment 1**  
+**Alessio Tommasi**
+
+**GitHub Repo**:  
+[https://github.com/AlessioTommasi-supsi/USI_MobileWearableComputing/tree/main/Lab12](https://github.com/AlessioTommasi-supsi/USI_MobileWearableComputing/tree/main/Lab12)
+
+---
+
+## **Components of Android Application**
+
+### **Activity**  
+An **Activity** represents a single screen with a user interface.  
+**Example from Lab01**:  
+
+- **Fig1 Location**: `/res/layout/activity_main.xml`
+- **Fig2 Location**: `/java/com/MainActivity.java`
+
+In the code, the Activity is linked in Java at line 27 during the `onCreate` method to ensure correct setup and usage.
+
+---
+
+### **Service**  
+A **Service** handles background operations without a user interface.  
+We did not use Services in Lab01, but in a Twitter-like app, they could be used for synchronizing notifications or downloading new tweets.
+
+---
+
+### **Broadcast Receiver**  
+A **Broadcast Receiver** allows the app to respond to system events or events from other apps.  
+We didn’t use this in Lab01 either, but an example could be limiting resource usage when the battery is low or resetting the counter when the app is put in the background.
+
+---
+
--- Lab12/app/src/main/java/com/example/lab1/MainActivity.java
+++ Lab12/app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean isActive = false;
 
     private TextView showCount;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         showCount = findViewById(R.id.Counter);
-
+        startButton = findViewById(R.id.Start);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,9 +37,11 @@ public class MainActivity extends AppCompatActivity {
     public void startCount(View view) {
         Toast toast = Toast.makeText(this, R.string.counterStarted, Toast.LENGTH_LONG);
         toast.show();
-
+        stepCounter = 0;
+        showCount.setText(String.format("%d", stepCounter));
         isActive = true;
-
+        startButton.setText(R.string.reset);
+        startButton.setOnClickListener(this::resetCount);
     }
 
     public void countUp(View view) {
@@ -45,5 +49,15 @@ public class MainActivity extends AppCompatActivity {
             stepCounter++;
             showCount.setText(String.format("%d", stepCounter));
         }
+    }
+
+    public void resetCount(View view) {
+        Toast toast = Toast.makeText(this, R.string.counterReset, Toast.LENGTH_LONG);
+        toast.show();
+        stepCounter = 0;
+        showCount.setText(String.format("%d", stepCounter));
+        isActive = false;
+        startButton.setText(R.string.Start);
+        startButton.setOnClickListener(this::startCount);
     }
 }
